b2e70aadfd353808c460c596674d3570df2a5fa4
Lala-web <64230609+Lala-web@users.noreply.github.com>
2020-11-23 13:41:03 -0600

    Add files via upload

---
+++ MyArrayDataException.java
@@ -0,0 +1,7 @@
+package lesson02;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+        super("Некорректные данные! " + message);
+    }
+}
